a9b5e97f546d007f4e3bf9c6dae7bc9d10e16e90
ViniciusAlvesGitHub <132413949+ViniciusAlvesGitHub@users.noreply.github.com>
2023-05-09 15:58:43 -0300

    adicionando exercicios java

---
+++ src/principal/Program.java
@@ -0,0 +1,111 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leia = new Scanner(System.in);
+		
+		
+		/*exercício1
+		int numero;
+		
+		System.out.println("Digite um número inteiro:");
+		numero = leia.nextInt();
+		
+		System.out.println("O Número informado foi: " + numero);
+		*/
+		
+		
+		
+		
+		/*exercício2
+		int num1;
+		int num2;
+		
+		System.out.print("Escolha um numero:");
+		num1 = leia.nextInt();
+		
+		System.out.print("Escolha outro numero:");
+		num2 = leia.nextInt();
+		
+		System.out.print("Resultado: " + (num1+num2));
+		*/
+		
+		
+		/*exercício3
+		double semestre1;
+		double semestre2;
+		double semestre3;
+		double semestre4;
+		
+		System.out.println("Nota 1:");
+		semestre1 = leia.nextDouble();
+		
+		System.out.println("Nota 2:");
+		semestre2 = leia.nextDouble();
+		
+		System.out.println("Nota 3:");
+		semestre3 = leia.nextDouble();
+		
+		System.out.println("Nota 4:");
+		semestre4 = leia.nextDouble();
+		
+		System.out.println("Média: " + (semestre1+semestre2+semestre3+semestre4)/4);
+		*/
+		
+		
+		
+		/*exercício4
+		double salarioPorHora, horasTrabalhadas, salarioTotal;
+		
+		System.out.println("Quanto você ganha por hora? ");
+        salarioPorHora = leia.nextDouble();
+
+        System.out.println("Quantas horas você trabalhou no mês de abril? ");
+        horasTrabalhadas = leia.nextDouble();
+
+        salarioTotal = salarioPorHora * horasTrabalhadas;
+
+        System.out.println("Seu salário total do mês de abril é R$" + salarioTotal);
+		
+		
+		
+		exercício4
+		double salarioPorHora, horasTrabalhadas, salarioTotal;
+		
+		System.out.println("Quanto você ganha por hora? ");
+        salarioPorHora = leia.nextDouble();
+
+        System.out.println("Quantas horas você trabalhou no mês de abril? ");
+        horasTrabalhadas = leia.nextDouble();
+
+        System.out.println("Seu salário total do mês de abril é R$" + (salarioPorHora*horasTrabalhadas));
+		*/
+		
+		
+		
+		/*exercício5
+		System.out.println("Digite a temperatura em graus Fahrenheit: ");
+        double fahrenheit = leia.nextDouble();
+
+        double celsius = 5 * ((fahrenheit - 32) / 9);
+
+        System.out.println("A temperatura em graus Celsius é: " + celsius);
+		*/
+		
+		
+		/*exercício6
+		System.out.println("Digite a temperatura em graus Celsius: ");
+        double celsius = leia.nextDouble();
+
+        double fahrenheit = (celsius * 9/5) + 32;
+
+        System.out.println("A temperatura em graus Fahrenheit é: " + fahrenheit);
+		*/
+		
+	}
+
+}
